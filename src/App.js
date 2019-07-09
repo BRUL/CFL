181bf1030c685739179f3306c0b61a28b4f832e7
BRUL <jonathan.dequeker@gmail.com>
2019-07-09 20:06:52 +0200

    text updates, margins and layout.

--- src/App.js
+++ src/App.js
@@ -16,15 +16,16 @@ class App extends Component {
     return (
       <div className="container-flex">
 
-        <div className="header row align-items-center">
+        <div className="header row align-items-center mb-5">
 
           <div className="col-md-3 offset-md-1 logo ">
             <img src={logo} alt="logo" />
           </div>
 
-          <div className="col-md mb-5">
-            <h1>Zitmeubelen van kwaliteit</h1>
+          <div className="col-md-7">
+            <h1>Zitmeubelen voor het leven</h1>
             <p className="lead">Comfort For Life is reeds meer dan 10&nbsp;jaar gespecialiseerd in salons en zitmeubelen van uitzonderlijke kwaliteit.</p>
+            <p className="lead"><a href="#contact" className="text-muted">Contacteer ons.</a></p>
           </div>
 
         </div>
@@ -35,9 +36,9 @@ class App extends Component {
             <div className="col-md-7 order-md-2 beeld">
               <img src={showbeeld1} className="w-100" alt="" />
             </div>
-            <div className="col-md-4 offset-md-1 text-right">
-              <h2 className="mt-md-3">Stijlvol leder</h2>
-              <p className="lead">Onze kennis van leder vertaald zich in een selectie van fabrikanten met know-how.</p>
+            <div className="col-md-4 offset-md-1 text-right-md my-3">
+              <h2 className="box">Stijlvol leder</h2>
+              <p className="lead">A a parturient a dictum ligula a porta suspendisse molestie purus libero.</p>
             </div>
           </div>
 
@@ -45,8 +46,8 @@ class App extends Component {
             <div className="col-md-7 beeld order-md-1">
               <img src={showbeeld2} className="w-100" alt="" />
             </div>
-            <div className="col-md-4 order-md-2 text-left">
-              <h2 className="mt-md-3">Ergonomie op maat</h2>
+            <div className="col-md-4 order-md-2 text-left my-3">
+              <h2 className="box box-white">Ergonomie op maat</h2>
               <p className="lead">Parturient eleifend eget ac eu a vestibulum tristique consectetur feugiat id scelerisque cum enim.</p>
             </div>
           </div>
@@ -55,8 +56,8 @@ class App extends Component {
             <div className="col-md-7 order-md-2 beeld">
               <img src={showbeeld3} className="w-100" alt="" />
             </div>
-            <div className="col-md-4 offset-md-1 text-right">
-              <h2 className="mt-md-3">Kwaliteit voorop</h2>
+            <div className="col-md-4 offset-md-1 text-right-md my-3">
+              <h2 className="box">Kwaliteit voorop</h2>
               <p className="lead">Parturient sit venenatis a pretium duis commodo vestibulum eu nibh fringilla mi consectetur in urna ante maecenas a dictum.</p>
             </div>
           </div>
@@ -70,11 +71,15 @@ class App extends Component {
             <div className="col-6 col-md"><img src={kasten3} alt="" className="w-100" /></div>
             <div className="col-6 col-md"><img src={kasten4} alt="" className="w-100" /></div>
           </div>
-          <h2 className="mt-md-3">Aandacht voor het geheel</h2>
-          <p className="lead">Parturient sit venenatis a pretium duis commodo vestibulum eu nibh fringilla mi consectetur in urna ante maecenas a dictum.</p>
+          <div className="my-3">
+            <h2>Aandacht voor het geheel</h2>
+            <p className="lead">Parturient sit venenatis a pretium duis commodo vestibulum eu nibh fringilla mi consectetur in urna ante maecenas a dictum.</p>
+          </div>
         </div>
 
-        <footer className="container py-5">
+        <a name="contact"></a>
+
+        <footer className="container py-5" itemscope itemtype="http://schema.org/LocalBusiness">
           <div className="row">
             <div className="col-12 col-md">
               <img src={logoS} className="icon" alt="" />
@@ -83,16 +88,16 @@ class App extends Component {
             <div className="col col-md">
               <h5>Bedrijf</h5>
               <ul className="list-unstyled text-small text-muted">
-                <li>Comfort&nbsp;For&nbsp;Life&nbsp;bvba</li>
+                <li itemprop="name">Comfort For Life bvba</li>
                 <li>BE 0889.754.274</li>
               </ul>
             </div>
             <div className="col col-md">
               <h5>Hoofdzetel</h5>
               <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="tel:+32 57 40 14 00">+32 57 40 14 00</a></li>
-                <li><a className="text-muted" href="mailto:info@comfortforlife.be">info@comfortforlife.be</a></li>
-                <li><a className="text-muted" href="https://goo.gl/maps/X7qnjzWCrYDdjNrj8">Izegrimstraat 1, 8900 Ieper</a></li>
+                <li><a className="text-muted" itemprop="telephone" href="tel:+32 57 40 14 00">+32 57 40 14 00</a></li>
+                <li><a className="text-muted" itemprop="email" href="mailto:info@comfortforlife.be">info@comfortforlife.be</a></li>
+                <li><a className="text-muted" itemprop="address" href="https://goo.gl/maps/X7qnjzWCrYDdjNrj8">Izegrimstraat 1, 8900 Ieper</a></li>
               </ul>
             </div>
           </div>
