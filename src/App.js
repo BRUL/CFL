00209259a6f8eabe316b5c47222a373b1b0b26e1
BRUL <jonathan.dequeker@gmail.com>
2019-07-09 18:31:42 +0200

    foto's kasten, contact

--- src/App.js
+++ src/App.js
@@ -1,140 +1,98 @@
 import React, { Component } from 'react';
 import logo from './logo.jpg';
+import logoS from './logo-s.png';
 import showbeeld1 from './showbeeld-preview-1.jpg';
+import showbeeld2 from './showbeeld-preview-2.jpg';
+import showbeeld3 from './showbeeld-preview-3.jpg';
+
+import kasten1 from './kasten-preview-1.jpg';
+import kasten2 from './kasten-preview-2.jpg';
+import kasten3 from './kasten-preview-3.jpg';
+import kasten4 from './kasten-preview-4.jpg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="App-header row p-5">
-          <div className="text-center col-12 col-md-5">
-            <img src={logo} className="w-100" />
+      <div className="container-flex">
+
+        <div className="header row align-items-center">
+
+          <div className="col-md-3 offset-md-1 logo ">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="col-11 col-md-7">
-            <h1 className="display-4">Zitmeubelen van kwaliteit</h1>
-            <p className="lead">Comfort For Life is reeds meer dan 10 jaar gespecialiseerd in salons en zitmeubelen van uitzonderlijke kwaliteit.</p>
+
+          <div className="col-md mb-5">
+            <h1>Zitmeubelen van kwaliteit</h1>
+            <p className="lead">Comfort For Life is reeds meer dan 10&nbsp;jaar gespecialiseerd in salons en zitmeubelen van uitzonderlijke kwaliteit.</p>
           </div>
+
         </div>
 
-        <div className="App-showbeelden row">
-          <div className="col-12 col-md-4  text-right">
-            <h2 className="display-5 m-md-4">Stijlvol leder</h2>
-            <p className="lead">Onze kennis van leder vertaald zich in een selectie van fabrikanten met know-how.</p>
+        <div className="showbeelden">
+
+          <div className="row align-items-center mb-5">
+            <div className="col-md-7 order-md-2 beeld">
+              <img src={showbeeld1} className="w-100" alt="" />
+            </div>
+            <div className="col-md-4 offset-md-1 text-right">
+              <h2 className="mt-md-3">Stijlvol leder</h2>
+              <p className="lead">Onze kennis van leder vertaald zich in een selectie van fabrikanten met know-how.</p>
+            </div>
           </div>
-          <div className="bg-light text-center col-12 col-md-8 p-0">
-            <img src={showbeeld1} className="w-100" />
+
+          <div className="row bg-dark text-white align-items-center mb-5">
+            <div className="col-md-7 beeld order-md-1">
+              <img src={showbeeld2} className="w-100" alt="" />
+            </div>
+            <div className="col-md-4 order-md-2 text-left">
+              <h2 className="mt-md-3">Ergonomie op maat</h2>
+              <p className="lead">Parturient eleifend eget ac eu a vestibulum tristique consectetur feugiat id scelerisque cum enim.</p>
+            </div>
           </div>
+
+          <div className="row align-items-center mb-5">
+            <div className="col-md-7 order-md-2 beeld">
+              <img src={showbeeld3} className="w-100" alt="" />
+            </div>
+            <div className="col-md-4 offset-md-1 text-right">
+              <h2 className="mt-md-3">Kwaliteit voorop</h2>
+              <p className="lead">Parturient sit venenatis a pretium duis commodo vestibulum eu nibh fringilla mi consectetur in urna ante maecenas a dictum.</p>
+            </div>
+          </div>
+
         </div>
 
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
+        <div className="kasten bg-light py-4">
+          <div className="row beelden no-gutters">
+            <div className="col-6 col-md"><img src={kasten1} alt="" className="w-100" /></div>
+            <div className="col-6 col-md"><img src={kasten2} alt="" className="w-100" /></div>
+            <div className="col-6 col-md"><img src={kasten3} alt="" className="w-100" /></div>
+            <div className="col-6 col-md"><img src={kasten4} alt="" className="w-100" /></div>
           </div>
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-dark shadow-sm mx-auto" ></div>
-          </div>
-          <div className="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-light shadow-sm mx-auto" ></div>
-          </div>
-        </div>
-
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-white shadow-sm mx-auto" ></div>
-          </div>
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-white shadow-sm mx-auto" ></div>
-          </div>
-        </div>
-
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-white shadow-sm mx-auto" ></div>
-          </div>
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-white shadow-sm mx-auto"></div>
-          </div>
+          <h2 className="mt-md-3">Aandacht voor het geheel</h2>
+          <p className="lead">Parturient sit venenatis a pretium duis commodo vestibulum eu nibh fringilla mi consectetur in urna ante maecenas a dictum.</p>
         </div>
 
         <footer className="container py-5">
           <div className="row">
             <div className="col-12 col-md">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
-              <small className="d-block mb-3 text-muted">© 2017-2019</small>
+              <img src={logoS} className="icon" alt="" />
+              <small className="d-block mb-3 text-muted">© 2007-2019</small>
             </div>
-            <div className="col-6 col-md">
-              <h5>Features</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Cool stuff</a></li>
-                <li><a className="text-muted" href="#">Random feature</a></li>
-                <li><a className="text-muted" href="#">Team feature</a></li>
-                <li><a className="text-muted" href="#">Stuff for developers</a></li>
-                <li><a className="text-muted" href="#">Another one</a></li>
-                <li><a className="text-muted" href="#">Last time</a></li>
+            <div className="col col-md">
+              <h5>Bedrijf</h5>
+              <ul className="list-unstyled text-small text-muted">
+                <li>Comfort&nbsp;For&nbsp;Life&nbsp;bvba</li>
+                <li>BE 0889.754.274</li>
               </ul>
             </div>
-            <div className="col-6 col-md">
-              <h5>Resources</h5>
+            <div className="col col-md">
+              <h5>Hoofdzetel</h5>
               <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Resource</a></li>
-                <li><a className="text-muted" href="#">Resource name</a></li>
-                <li><a className="text-muted" href="#">Another resource</a></li>
-                <li><a className="text-muted" href="#">Final resource</a></li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>Resources</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Business</a></li>
-                <li><a className="text-muted" href="#">Education</a></li>
-                <li><a className="text-muted" href="#">Government</a></li>
-                <li><a className="text-muted" href="#">Gaming</a></li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>About</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Team</a></li>
-                <li><a className="text-muted" href="#">Locations</a></li>
-                <li><a className="text-muted" href="#">Privacy</a></li>
-                <li><a className="text-muted" href="#">Terms</a></li>
+                <li><a className="text-muted" href="tel:+32 57 40 14 00">+32 57 40 14 00</a></li>
+                <li><a className="text-muted" href="mailto:info@comfortforlife.be">info@comfortforlife.be</a></li>
+                <li><a className="text-muted" href="https://goo.gl/maps/X7qnjzWCrYDdjNrj8">Izegrimstraat 1, 8900 Ieper</a></li>
               </ul>
             </div>
           </div>
