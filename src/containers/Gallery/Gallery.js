d07ef969fa526ed28b6f96e19d13b178f2e8e1af
BRUL <jonathan.dequeker@gmail.com>
2019-08-26 22:52:13 +0200

    responsive images

--- src/containers/Gallery/Gallery.js
+++ src/containers/Gallery/Gallery.js
@@ -2,28 +2,9 @@ import React, { Component } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import LazyLoad from 'react-lazyload';
 import Carousel from 'react-bootstrap/Carousel'
+import ResponsiveImage from '../../components/ResponsiveImage/ResponsiveImage'
+
 import './Gallery.scss';
-
-/*
-* images
-*/
-import modelAruba1 from '../../img/modelAruba1.jpg';
-import modelAruba2 from '../../img/modelAruba2.jpg';
-import modelAruba3 from '../../img/modelAruba3.jpg';
-import modelAruba4 from '../../img/modelAruba4.jpg';
-import modelLausanne1 from '../../img/modelLausanne1.jpg';
-import modelLausanne2 from '../../img/modelLausanne2.jpg';
-import modelLissabon1 from '../../img/modelLissabon1.jpg';
-import modelLissabon2 from '../../img/modelLissabon2.jpg';
-import modelLissabon3 from '../../img/modelLissabon3.jpg';
-import modelLissabon4 from '../../img/modelLissabon4.jpg';
-import modelLissabon5 from '../../img/modelLissabon5.jpg';
-import modelSevilla1 from '../../img/modelSevilla1.jpg';
-import modelSevilla2 from '../../img/modelSevilla2.jpg';
-import modelSevilla3 from '../../img/modelSevilla3.jpg';
-import modelSevilla4 from '../../img/modelSevilla4.jpg';
-import modelSevilla5 from '../../img/modelSevilla5.jpg';
-
 
 class Gallery extends Component {
   render() {
@@ -46,56 +27,55 @@ class Gallery extends Component {
           </div>
         </div>
 
-        <div className="row modelRow my-3" id="aruba">
+        <div className="row modelRow my-3 align-items-center" id="aruba">
           <div className="col-md-3 offset-md-1">
             <h3>Aruba</h3>
-            <p>Eventueel kan hier wat uitleg komen over dit model.</p>
           </div>
           <LazyLoad height="66vw" offset={100}>
             <Carousel indicators={true} interval={60000} className="col-md-7">
-              <Carousel.Item><img src={modelAruba1} alt="model Aruba beeld 1" /></Carousel.Item>
-              <Carousel.Item><img src={modelAruba2} alt="model Aruba beeld 2"/></Carousel.Item>
-              <Carousel.Item><img src={modelAruba3} alt="model Aruba beeld 3"/></Carousel.Item>
-              <Carousel.Item><img src={modelAruba4} alt="model Aruba beeld 4"/></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Aruba1" alt="model Aruba beeld 1" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Aruba2" alt="model Aruba beeld 2" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Aruba3" alt="model Aruba beeld 3" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Aruba4" alt="model Aruba beeld 4" title="" /></Carousel.Item>
             </Carousel>
           </LazyLoad>
         </div>
-        <div className="row modelRow my-3" id="lausanne">
+        <div className="row modelRow my-3 align-items-center" id="lausanne">
           <div className="col-md-3 offset-md-1">
             <h3>Lausanne</h3>
           </div>
           <LazyLoad height="66vw" offset={100}>
             <Carousel indicators={true} interval={60000} className="col-md-7">
-              <Carousel.Item><img src={modelLausanne1} alt="model Lausanne beeld 1"/></Carousel.Item>
-              <Carousel.Item><img src={modelLausanne2} alt="model Lausanne beeld 2"/></Carousel.Item>
+            <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lausanne1" alt="model Lausanne beeld 1" title="" /></Carousel.Item>
+            <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lausanne2" alt="model Lausanne beeld 2" title="" /></Carousel.Item>
             </Carousel>
           </LazyLoad>
         </div>
-        <div className="row modelRow my-3" id="lissabon">
+        <div className="row modelRow my-3 align-items-center" id="lissabon">
           <div className="col-md-3 offset-md-1">
             <h3>Lissabon</h3>
           </div>
           <LazyLoad height="66vw" offset={100}>
             <Carousel indicators={true} interval={60000} className="col-md-7">
-              <Carousel.Item><img src={modelLissabon1} alt="model Lissabon beeld 1"/></Carousel.Item>
-              <Carousel.Item><img src={modelLissabon2} alt="model Lissabon beeld 2"/></Carousel.Item>
-              <Carousel.Item><img src={modelLissabon3} alt="model Lissabon beeld 3"/></Carousel.Item>
-              <Carousel.Item><img src={modelLissabon4} alt="model Lissabon beeld 4"/></Carousel.Item>
-              <Carousel.Item><img src={modelLissabon5} alt="model Lissabon beeld 5"/></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lissabon1" alt="model Lissabon beeld 1" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lissabon2" alt="model Lissabon beeld 2" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lissabon3" alt="model Lissabon beeld 3" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lissabon4" alt="model Lissabon beeld 4" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Lissabon5" alt="model Lissabon beeld 5" title="" /></Carousel.Item>
             </Carousel>
           </LazyLoad>
         </div>
-        <div className="row modelRow my-3" id="sevilla">
+        <div className="row modelRow my-3 align-items-center" id="sevilla">
           <div className="col-md-3 offset-md-1">
             <h3>Sevilla</h3>
           </div>
           <LazyLoad height="66vw" offset={100}>
             <Carousel indicators={true} interval={60000} className="col-md-7">
-              <Carousel.Item><img src={modelSevilla1} alt="model Sevilla beeld 1"/></Carousel.Item>
-              <Carousel.Item><img src={modelSevilla2} alt="model Sevilla beeld 2"/></Carousel.Item>
-              <Carousel.Item><img src={modelSevilla3} alt="model Sevilla beeld 3"/></Carousel.Item>
-              <Carousel.Item><img src={modelSevilla4} alt="model Sevilla beeld 4"/></Carousel.Item>
-              <Carousel.Item><img src={modelSevilla5} alt="model Sevilla beeld 5"/></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Sevilla1" alt="model Sevilla beeld 1" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Sevilla2" alt="model Sevilla beeld 2" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Sevilla3" alt="model Sevilla beeld 3" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Sevilla4" alt="model Sevilla beeld 4" title="" /></Carousel.Item>
+              <Carousel.Item><ResponsiveImage projectName="catalogus" imageName="Sevilla5" alt="model Sevilla beeld 5" title="" /></Carousel.Item>
             </Carousel>
           </LazyLoad>
         </div>
